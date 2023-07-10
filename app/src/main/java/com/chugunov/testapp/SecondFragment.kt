6ab6daedced956a6382d9efccfa5448d426b839d
Dmitriy Chugunov <yolvissplay@gmail.com>
2023-07-10 20:42:23 +0300

    created SecondFragment.kt

---
+++ app/src/main/java/com/chugunov/testapp/SecondFragment.kt
@@ -0,0 +1,29 @@
+package com.chugunov.testapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.chugunov.testapp.databinding.FragmentSecondBinding
+
+class SecondFragment: Fragment() {
+
+    private var _binding: FragmentSecondBinding? = null
+    private val binding: FragmentSecondBinding
+        get() = _binding ?: throw RuntimeException("FragmentSecondBinding = null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
